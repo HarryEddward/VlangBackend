6f07f59a437be825f4127fb9af05bf7a0cece30c
HarryEddward <he0780070@gmail.com>
2024-08-27 15:53:13 +0200

    changes

--- backend/main.v
+++ backend/main.v
@@ -1,20 +1,11 @@
 module main
 
-
-// Definición de la estructura Persona
-struct Persona {
-    nombre string
-    edad   int
-}
-
-// Método asociado a la estructura Persona
-fn (p Persona) hablar(frase string) string {
-    return '$p.nombre dice: $frase'
-}
+import fmt
 
 // Función principal
 fn main() {
-    persona := Persona{nombre: 'Vivian', edad: 22}
-    saludo := persona.hablar('¡Vlang es increíble!')
-    println(saludo)
+    mut edad := 25
+	var nombre string = "adrian"
+
+	println("$nombre: $edad")
 }
